66cb410b06d5c836100e62283fbf8c534d45c6cd
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-04-03 17:06:51 -0300

    Step 11

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 const api = "https://pomber.github.io/covid19/";
 const DATA = api + "timeseries.json";
+const FLAGS = api + "countries.json";
 
 import fetch from "node-fetch";
 
 export async function getStaticProps() {
-  const response = await fetch(DATA);
-  const data = await response.json();
+  const [data, flags] = await Promise.all([
+    fetch(DATA).then((r) => r.json()),
+    fetch(FLAGS).then((r) => r.json()),
+  ]);
   const countries = Object.keys(data);
   const aCountry = data[countries[0]];
   const { date } = aCountry[aCountry.length - 1];
   const rows = countries
     .map((country) => {
       const { deaths } = data[country].find((r) => r.date === date);
-      return { country, deaths };
+      const flag = flags[country]?.flag || "❓";
+      return { country, deaths, flag };
     })
     .filter((r) => r.deaths > 8);
   return {
