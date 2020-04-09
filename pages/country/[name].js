6c3dd0ef8eb98f7d7b98ac31c9d716520c0888c3
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-04-09 10:41:27 -0300

    Step 15

--- pages/country/[name].js
+++ pages/country/[name].js
@@ -1,9 +1,27 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+import fetch from "node-fetch";
 
-export default function Country() {
-  const router = useRouter();
-  const { name } = router.query;
+const api = "https://pomber.github.io/covid19/";
+const DATA = api + "timeseries.json";
+
+export async function getStaticPaths() {
+  const response = await fetch(DATA);
+  const data = await response.json();
+  const countries = Object.keys(data);
+  return {
+    paths: countries.map((name) => ({
+      params: { name },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { name } = context.params;
+  return { props: { name } };
+}
+
+export default function Country({ name }) {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>{name}</h1>
